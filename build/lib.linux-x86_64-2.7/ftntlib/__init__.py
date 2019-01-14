aee3aa298f2077cc152c4eae2bb56a0a9d42ca0d
Jean-Pierre Forcioli <jpforcioli@fortinet.com>
2019-01-14 15:25:03 -0700

    0.4.0.dev16

--- build/lib.linux-x86_64-2.7/ftntlib/__init__.py
+++
@@ -1,3 +0,0 @@
-from .fmg_jsonapi import FortiManagerJSON
-from .fmg_xmlapi import FortiManagerXML
-from .fos_restapi import FortiOSREST
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
       name='ftntlib',
-      version='0.4.0.dev15',
+      version='0.4.0.dev16',
       description='Python modules to interact with Fortinet products',
       install_requires=['requests','suds-jurko','lxml'],
       author='Original: Ashton Turpin, Maintainer: Jean-Pierre Forcioli',
