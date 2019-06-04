e6199e8333a04e100b2634a36c61e9a5ade02936
Jean-Pierre Forcioli <jpforcioli@fortinet.com>
2019-06-04 18:47:17 +0200

    0.4.0.dev18

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup
 
 setup(
       name='ftntlib',
-      version='0.4.0.dev17',
+      version='0.4.0.dev18',
       description='Python modules to interact with Fortinet products',
       install_requires=['requests','suds-jurko','lxml'],
-      author='Original: Ashton Turpin, Maintainer: Jean-Pierre Forcioli',
-      author_email='jpforcioli@fortinet.com',
+      author='Original: Ashton Turpin, Maintainer: Jean-Pierre Forcioli, Contributors: Jeremy Parente',
+      author_email='jpforcioli@fortinet.com, jparente@fortinet.com',
       url='https://fndn.fortinet.net',
       packages=['ftntlib'],
       )
