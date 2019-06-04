4c749210c971c17a5b7ba30f19b63d88d84d3d2f
Jean-Pierre Forcioli <jpforcioli@fortinet.com>
2019-06-04 18:52:27 +0200

    to support fap_restapi

--- ftntlib/__init__.py
+++ ftntlib/__init__.py
@@ -3,3 +3,4 @@ from .faz_jsonapi import FortiAnalyzerJSON
 from .fmg_xmlapi import FortiManagerXML
 from .fos_restapi import FortiOSREST
 from .fpc_restapi import FortiPortalREST
+from .fap_restapi import FAPREST
