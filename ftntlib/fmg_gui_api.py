ff0ca04b6838fdb40b05e07c7d8cbe921bab901b
Jean-Pierre Forcioli <jpforcioli@fortinet.com>
2020-06-30 09:55:08 +0200

    added support for xsrf-token in fmg_gui_api.py

--- ftntlib/fmg_gui_api.py
+++ ftntlib/fmg_gui_api.py
@@ -1,5 +1,13 @@
+import sys
+import logging
 import requests
+import json
 
+if sys.version_info >= (2,7):
+    logging.captureWarnings(True)
+else:
+    from requests.packages.urllib3.exceptions import InsecureRequestWarning
+    requests.packages.urllib3.disable_warnings()
 class FmgGuiApi():
     def __init__(self):
         self._proto = 'https'
@@ -8,21 +16,92 @@ class FmgGuiApi():
         self._base_url = None 
         self._web_session = requests.Session()
         self._debug = None
+        self._debug_cookie = None
+        self._debug_header = None
 
-    def debug(value):
+    def debug(self, value):
         if value == 'on':
             self._debug = True
-        else
+        else:
             self._debug = False
 
         return self._debug
 
-    def login(self, host, login, password, port=''):
+    def debug_cookie(self, value):
+        if value == 'on':
+            self._debug_cookie = True
+        else:
+            self._debug_cookie = False
+
+        return self._debug_cookie
+
+    def debug_header(self, value):
+        if value == 'on':
+            self._debug_header = True
+        else:
+            self._debug_header = False
+
+        return self._debug_header
+
+    def debug_print(self, response):
+        if self._debug:
+            # REQUEST
+            method = response.request.method
+            url = response.request.url
+            body = response.request.body
+
+            print('>>>')
+            print('{} {}'.format(method, url))
+
+            if body:
+                print('\n{}'.format(json.dumps(json.loads(body), indent=4)))
+
+            if self._debug_header:
+                print('>>> [headers]')
+                for header in self._web_session.headers:
+                    print('{}: {}'.format(header, self._web_session.headers[header]))
+
+            if self._debug_cookie:
+                print('>>> [cookies]')
+                for cookie in self._web_session.cookies:
+                    print(cookie)
+
+            # RESPONSE
+            status_code = response.status_code
+            content_type = response.headers.get('content-type')
+
+            print('<<<')
+            print('{}'.format(status_code))
+
+            if content_type == 'application/json':
+                print('\n{}'.format(json.dumps(json.loads(response.text), indent=4)))
+            else:
+                print('\n{}'.format(response.text))
+
+            if self._debug_cookie:
+                print('<<< [cookies]')
+                for cookie in response.cookies:
+                    print(cookie)
+
+    def set_headers(self, response):
+        x_csrftoken = {
+            'X-CSRFToken': self._web_session.cookies['csrftoken']
+        }
+        #self._web_session.headers.update(x_csrftoken)
+
+        xsrf_token = {
+            'XSRF-TOKEN': self._web_session.cookies['XSRF-TOKEN']
+        }
+        self._web_session.headers.update(xsrf_token)        
+
+        #self._web_session.headers.update(x_xsrf_token)                
+
+    def login(self, host, login, password, port=443):
         self._host = host
         self._port = port
-        self._base_url = '{}://{}:{}/'.format(proto,
-                                              host,
-                                              port)
+        self._base_url = '{}://{}:{}'.format(self._proto,
+                                              self._host,
+                                              self._port)
 
         login_url1 = '{}/cgi-bin/module/flatui_auth'.format(self._base_url)
         login_url2 = '{}/p/app/'.format(self._base_url)
@@ -38,7 +117,22 @@ class FmgGuiApi():
         }
 
         ## This request will retrieve the CURRENT_SESSION and HTTP_CSRF_TOKEN cookies 
-        response = self._web_session.post(login_url1, json=request_body)
+        response = self._web_session.post(login_url1, json=request_body, verify=False)
+        self.debug_print(response)
 
         ## This request will retrieve the XSRF-TOKEN and csrftoken cookies
-        response = self._web_session.get(login_url2)
+        response = self._web_session.get(login_url2, verify=False)
+        self.debug_print(response)
+
+        ## Set X-CSRFToken, XSR-TOKEN and X-XSRF-TOKEN
+        self.set_headers(response)
+
+    def flatui_proxy(self, method, params=None, json=None):
+        url = '{}/cgi-bin/module/flatui_proxy'.format(self._base_url)
+
+        if method == 'get':
+            response = self._web_session.get(url, params=params)
+
+        elif method == 'post':
+            response = self._web_session.post(url, params=params, json=json)
+            self.debug_print(response)
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
       name='ftntlib',
-      version='0.4.0.dev19',
+      version='0.4.0.dev20',
       description='Python modules to interact with Fortinet products',
       install_requires=['requests','suds-jurko','lxml'],
       author='Original: Ashton Turpin, Maintainer: Jean-Pierre Forcioli, Contributors: Jeremy Parente',
