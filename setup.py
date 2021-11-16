da5f2c15ed0208ca12b892848d12c14778f040ba
Dimitri <dimitri.segard@gmail.com>
2021-11-16 15:35:10 +0100

    Replace suds-jurko by suds-community

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from setuptools import setup
 
 setup(
       name='ftntlib',
-      version='0.4.0.dev20',
+      version='0.4.0.dev21',
       description='Python modules to interact with Fortinet products',
-      install_requires=['requests','suds-jurko','lxml'],
+      install_requires=['requests','suds-community','lxml'],
       author='Original: Ashton Turpin, Maintainer: Jean-Pierre Forcioli, Contributors: Jeremy Parente',
       author_email='jpforcioli@fortinet.com, jparente@fortinet.com',
       url='https://fndn.fortinet.net',
